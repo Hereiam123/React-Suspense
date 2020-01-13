b8f2ffab50664f2e6210b44d80f236643f4185dd
206620219 <Brian.DeMaio@universalorlando.com>
2020-01-13 16:51:08 -0500

    Setting up to output returned data in components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.css";
+import { fetchData } from "./API";
+
+const resource = fetchData();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Here you go!</h1>
-      </header>
+    <div className="container my-5">
+      <h1>Here you go!</h1>
+      <ProfileDeatils />
     </div>
   );
 }
+
+const ProfileDetails = () => {
+  const user = resource.user.read();
+  return (
+    <div className="card card-body my-3">
+      <h1 className="large text-primary">{user.name}</h1>
+      <ul>
+        <li>Username: {user.name}</li>
+        <li>Email: {user.email}</li>
+        <li>City: {user.address.city}</li>
+      </ul>
+    </div>
+  );
+};
+
+const ProfilePosts = () => {
+  const posts = resource.posts.read();
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">
+        <strong>Latest Post</strong>
+      </li>
+      {posts.map(post => (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default App;
